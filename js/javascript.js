c8bdafe6dec07124a0e07cdb1a87a123d74f6d1d
Your Name <youremail@yourdomain.com>
2023-06-30 05:50:01 -0700

    shooter function left/right

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
 
 const grid = document.querySelector('.grid');
+let currentShooterIndex = 202;
+let width = 15;
 
 for(let i = 0; i < 225; i++) {
     const square = document.createElement('div');
@@ -20,3 +22,19 @@ const alienInvaders = [
     }
 
     draw();
+
+    squares[currentShooterIndex].classList.add('shooter');
+
+    function moveShooter(e) {
+        squares[currentShooterIndex].classList.remove('shooter');
+        switch(e.key) {
+            case 'ArrowLeft':
+                if(currentShooterIndex % width !== 0) currentShooterIndex -= 1;
+                break
+            case 'ArrowRight':
+                if(currentShooterIndex % width < width -1) currentShooterIndex += 1;
+                break
+        }
+        squares[currentShooterIndex].classList.add('shooter');
+    }
+    document.addEventListener('keydown', moveShooter);
